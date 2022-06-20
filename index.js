36d140eb9a0fcc9df38c3c4a83d3da96473e761b
Andre Moraes <andre.moraes.98@gmail.com>
2022-06-20 18:22:23 -0300

    Correação do lint

--- index.js
+++ index.js
@@ -12,30 +12,38 @@ const PORT = '3000';
 const validateEmail = (req, res, next) => {
   const { email } = req.body;
 
-  if (!email) return res
+  if (!email) {
+    return res
     .status(400)
     .json({ message: 'O campo "email" é obrigatório' });
+  }
 
-  if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) return res
+  if (!(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/).test(email)) {
+    return res
     .status(400)
     .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
 
   next();
-}
+};
 
 const validatePassword = (req, res, next) => {
   const { password } = req.body;
 
-  if (!password) return res
+  if (!password) {
+    return res
     .status(400)
     .json({ message: 'O campo "password" é obrigatório' });
+  }
 
-  if (password.length < 6) return res
+  if (password.length < 6) {
+    return res
     .status(400)
     .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
 
   next();
-}
+};
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -59,13 +67,13 @@ app.get('/talker/:id', async (req, res) => {
   const jsonRegisteredPeople = JSON.parse(registeredPeople);
 
   const registeredPerson = jsonRegisteredPeople
-    .find((person) => person.id === Number(id))
+    .find((person) => person.id === Number(id));
   
   if (registeredPerson) {
     return res.status(200).json(registeredPerson);
   }
 
-  res.status(404).json({ "message": "Pessoa palestrante não encontrada" });  
+  res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 app.post('/login', validateEmail, validatePassword, async (_req, res) => {
